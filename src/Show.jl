52e8b04b45efd0ee67ead510ceb82d42d0a3e56a
Cris Moen <cris.moen@runtosolve.com>
2024-07-13 16:13:41 +0300

    revise weld equation J26

--- src/Show.jl
+++ src/Show.jl
@@ -2,6 +2,30 @@ module Show
 
 using LinearAlgebra, CairoMakie, LinesCurvesNodes
 
+
+struct SignatureCurveOptions
+
+    drawing_size::Vector{Float64}
+    backgroundcolor::Symbol
+    linecolor::Symbol
+    linewidth::Float64
+    fontsize::Float64
+    markersize::Float64
+
+end
+
+struct ModeShapeOptions
+
+    drawing_size::Vector{Float64}
+    thickness_scale::Float64
+    backgroundcolor::Symbol
+    linecolor::Symbol
+    linestyle::Symbol
+    joinstyle::Symbol
+
+end
+
+
 using ..Tools 
 
 function define_element_global_dof(elem, node)
@@ -152,7 +176,7 @@ function show_element_deformed_shape!(ax, element_XY, Δ, scale, attributes, lin
     ΔY = [Δ[i][2] for i in eachindex(element_XY)]
 
 
-    lines!(ax, X .+ scale[1] * ΔX, Y .+ scale[2] * ΔY, linestyle=attributes.linestyle, color=attributes.color, linewidth=line_thickness)
+    lines!(ax, X .+ scale[1] * ΔX, Y .+ scale[2] * ΔY, linestyle=attributes.linestyle, color=attributes.color, linewidth=line_thickness, joinstyle=attributes.joinstyle, linecap=attributes.linecap)
 
     # for i=1:(length(X)-1)
 
@@ -162,111 +186,281 @@ function show_element_deformed_shape!(ax, element_XY, Δ, scale, attributes, lin
 
 end
 
-function signature_curve(model, eig, scale)
+function signature_curve(model, eig, options)
 
     Pcr = Tools.get_load_factor(model, eig)
-    figure = Figure(size = (4.0*72, 4.0*72))
-    ax = Axis(figure[1, 1])
-    [scatterlines!(model.lengths .* scale[1], Pcr .* scale[2], color=:blue) for i in eachindex(Pcr)];
+    figure = Figure(size = (options.drawing_size[1], options.drawing_size[2]), backgroundcolor=options.backgroundcolor, fontsize = options.fontsize)
+    ax = Axis(figure[1, 1], backgroundcolor=options.backgroundcolor)
+    [scatterlines!(model.lengths, Pcr, color=options.linecolor, markersize=options.markersize, linewidth = options.linewidth) for i in eachindex(Pcr)];
+    
+    return ax, figure 
+
+end
+
+function signature_curve(model, eig, options, figure)
+
+    Pcr = Tools.get_load_factor(model, eig)
+    # figure = Figure(size = (options.drawing_size[1], options.drawing_size[2]), backgroundcolor=options.backgroundcolor, fontsize = options.fontsize)
+    ax = Axis(figure[1, 1], backgroundcolor=options.backgroundcolor)
+    [scatterlines!(model.lengths, Pcr, color=options.linecolor, markersize=options.markersize, linewidth = options.linewidth) for i in eachindex(Pcr)];
     
     return ax, figure 
 
 end
 
 #function signature_curve(model, eig, scale; figure = Figure())
-function signature_curve(model, eig, scale, figure)
+# function signature_curve(model, eig, scale, figure)
 
-    Pcr = Tools.get_load_factor(model, eig)
-    # figure = Figure(resolution = (4.0*72, 4.0*72))
-    ax = Axis(figure[1, 1])
-    [scatterlines!(model.lengths .* scale[1], Pcr .* scale[2], color=:blue) for i in eachindex(Pcr)];
+#     Pcr = Tools.get_load_factor(model, eig)
+#     # figure = Figure(resolution = (4.0*72, 4.0*72))
+#     ax = Axis(figure[1, 1])
+#     [scatterlines!(model.lengths .* scale[1], Pcr .* scale[2], color=:blue) for i in eachindex(Pcr)];
     
-    return ax, figure 
+#     return ax, figure 
 
-end
+# end
 
 
-function minimum_mode_shape(model, eig, t, deformation_scale, drawing_scale)
+function minimum_mode_shape(model, eig, t, deformation_scale, options)
     
-    # x = model.node[:, 2]
-    # y = model.node[:, 3]
-    # Δx = abs(maximum(x) - minimum(x))
-    # Δy = abs(maximum(y) - minimum(y))
 
     Pcr = Tools.get_load_factor(model, eig)
     mode_index = argmin(Pcr)
     mode = model.shapes[mode_index][:, eig]
 
-    n = fill(5, length(t))
+    n = fill(10, length(t))
 
     cross_section_coords, Δ, figure_max_dims = cross_section_mode_shape_info(model.elem, model.node, mode, n, deformation_scale)
 
-    linesegment_ranges, t_segments = LinesCurvesNodes.find_linesegments(t)
-    coords_as_linesegments = LinesCurvesNodes.combine_points_into_linesegments(linesegment_ranges, cross_section_coords)
-    Δ_as_linesegments = LinesCurvesNodes.combine_points_into_linesegments(linesegment_ranges, Δ)
-   
-    Δx = figure_max_dims[1]
-    Δy = figure_max_dims[2]
+    XY_discretized =Float64[]
+    t_discretized = Float64[]
+    Δ_discretized = Float64[]
 
-    figure = Figure(size = (Δx*72, Δy*72) .* drawing_scale)
-    ax = Axis(figure[1, 1], aspect = Δx/Δy)
+    for i in eachindex(cross_section_coords)
+
+        if i != size(cross_section_coords)[1]
+            XY_segment = [cross_section_coords[i][j] for j in eachindex(cross_section_coords[i])][1:end-1]
+        else
+            XY_segment = [cross_section_coords[i][j] for j in eachindex(cross_section_coords[i])] #last segment, add end node 
+        end
+
+        XY_discretized = vcat(XY_discretized, XY_segment)
+
+        t_discretized = vcat(t_discretized, fill(t[i], n[i]-1))
+
+        if i != size(cross_section_coords)[1]
+            Δ_segment = [Δ[i][j] for j in eachindex(Δ[i])][1:end-1]
+        else
+            Δ_segment = [Δ[i][j] for j in eachindex(Δ[i])]  #last segment, add end node 
+        end
+
+        Δ_discretized = vcat(Δ_discretized, Δ_segment)
+
+    end
+
+    X_discretized = [XY_discretized[i][1] for i in eachindex(XY_discretized)]
+    Y_discretized = [XY_discretized[i][2] for i in eachindex(XY_discretized)]
+
+    ΔX_discretized = [Δ_discretized[i][1] for i in eachindex(Δ_discretized)]
+    ΔY_discretized = [Δ_discretized[i][2] for i in eachindex(Δ_discretized)]
+
+    linesegment_ranges, t_segments = LinesCurvesNodes.find_linesegments(t_discretized)
+    coords_as_linesegments = LinesCurvesNodes.combine_points_into_linesegments(linesegment_ranges, X_discretized, Y_discretized)
+    Δ_as_linesegments = LinesCurvesNodes.combine_points_into_linesegments(linesegment_ranges, ΔX_discretized, ΔY_discretized)
+
+
+    # figure_span_x = figure_max_dims[1]
+    # figure_span_y = figure_max_dims[2]
+
+    # figure = Figure(size = (figure_span_x*72, figure_span_y*72) .* drawing_scale)
+    figure = Figure(size = (options.drawing_size[1], options.drawing_size[2]), backgroundcolor=options.backgroundcolor)
+    # ax = Axis(figure[1, 1], aspect = figure_span_x/figure_span_y)
+
+    ax = Axis(figure[1, 1], backgroundcolor = options.backgroundcolor)
+
+
     hidedecorations!(ax)  # hides ticks, grid and lables
     hidespines!(ax)  # hide the frame
     # thickness_scale = maximum(t) * 72 * drawing_scale
     # linewidths = t ./ maximum(t) * thickness_scale
 
-    thickness_scale = maximum(t_segments) * 72 * drawing_scale
-    linewidths = t_segments ./ maximum(t_segments) * thickness_scale
-    
-    attributes = (color=:grey, linestyle=:solid)
+    # thickness_scale = maximum(t_segments) * 72 * drawing_scale
+    linewidths = t_segments .* options.thickness_scale
+
+    attributes = (color=options.linecolor, linestyle=options.linestyle, joinstyle=options.joinstyle, linecap=:butt)
 
     # linesegment_ranges = LinesCurvesNodes.find_line_segments(linewidths)
 
     # [show_element_deformed_shape!(ax, cross_section_coords[i], Δ[i], deformation_scale, attributes, attributes.linewidth[i]) for i in eachindex(Δ)];
-   
+
     [show_element_deformed_shape!(ax, coords_as_linesegments[i], Δ_as_linesegments[i], deformation_scale, attributes, linewidths[i]) for i in eachindex(Δ_as_linesegments)];
 
-    # cross_section_mode_shape!(ax, model.elem, model.node, mode, n, deformation_scale, attributes);
+    num_nodes = size(model.node)[1]
+    num_elem = size(model.elem)[1]
+
+    # if length(X_discretized) == length(t_discretized)  #closed section
+    if num_nodes == num_elem
+
+        coords_last_linesegment = Vector{Vector{Float64}}(undef, 3)
+        Δ_last_linesegment = Vector{Vector{Float64}}(undef, 3)
+
+        coords_last_linesegment[1] = coords_as_linesegments[end][end]
+        coords_last_linesegment[2] = coords_as_linesegments[1][1]
+        coords_last_linesegment[3] = coords_as_linesegments[1][2]
+
+        Δ_last_linesegment[1] = Δ_as_linesegments[end][end]
+        Δ_last_linesegment[2] = Δ_as_linesegments[1][1]
+        Δ_last_linesegment[3] = Δ_as_linesegments[1][2]
+
+        attributes = (color=options.linecolor, linestyle=options.linestyle, joinstyle=options.joinstyle, linecap=:round) #just for connecting element
+
+
+        show_element_deformed_shape!(ax, coords_last_linesegment, Δ_last_linesegment, deformation_scale, attributes, linewidths[end])
+
+    end
     
     return ax, figure
 
 end
 
 
-function minimum_mode_shape(model, eig, t, deformation_scale, drawing_scale, figure)
+
+function minimum_mode_shape(model, eig, t, deformation_scale, options, figure)
     
-    # x = model.node[:, 2]
-    # y = model.node[:, 3]
-    # Δx = abs(maximum(x) - minimum(x))
-    # Δy = abs(maximum(y) - minimum(y))
 
     Pcr = Tools.get_load_factor(model, eig)
     mode_index = argmin(Pcr)
     mode = model.shapes[mode_index][:, eig]
 
-    n = fill(5, length(t))
+    n = fill(10, length(t))
 
     cross_section_coords, Δ, figure_max_dims = cross_section_mode_shape_info(model.elem, model.node, mode, n, deformation_scale)
-   
-    Δx = figure_max_dims[1]
-    Δy = figure_max_dims[2]
 
-    # figure = Figure(resolution = (Δx*72, Δy*72) .* drawing_scale)
-    ax = Axis(figure[1, 1], aspect = Δx/Δy)
+    XY_discretized =Float64[]
+    t_discretized = Float64[]
+    Δ_discretized = Float64[]
+
+    for i in eachindex(cross_section_coords)
+
+        if i != size(cross_section_coords)[1]
+            XY_segment = [cross_section_coords[i][j] for j in eachindex(cross_section_coords[i])][1:end-1]
+        else
+            XY_segment = [cross_section_coords[i][j] for j in eachindex(cross_section_coords[i])] #last segment, add end node 
+        end
+
+        XY_discretized = vcat(XY_discretized, XY_segment)
+
+        t_discretized = vcat(t_discretized, fill(t[i], n[i]-1))
+
+        if i != size(cross_section_coords)[1]
+            Δ_segment = [Δ[i][j] for j in eachindex(Δ[i])][1:end-1]
+        else
+            Δ_segment = [Δ[i][j] for j in eachindex(Δ[i])]  #last segment, add end node 
+        end
+
+        Δ_discretized = vcat(Δ_discretized, Δ_segment)
+
+    end
+
+    X_discretized = [XY_discretized[i][1] for i in eachindex(XY_discretized)]
+    Y_discretized = [XY_discretized[i][2] for i in eachindex(XY_discretized)]
+
+    ΔX_discretized = [Δ_discretized[i][1] for i in eachindex(Δ_discretized)]
+    ΔY_discretized = [Δ_discretized[i][2] for i in eachindex(Δ_discretized)]
+
+    linesegment_ranges, t_segments = LinesCurvesNodes.find_linesegments(t_discretized)
+    coords_as_linesegments = LinesCurvesNodes.combine_points_into_linesegments(linesegment_ranges, X_discretized, Y_discretized)
+    Δ_as_linesegments = LinesCurvesNodes.combine_points_into_linesegments(linesegment_ranges, ΔX_discretized, ΔY_discretized)
+
+
+    # figure_span_x = figure_max_dims[1]
+    # figure_span_y = figure_max_dims[2]
+
+    # figure = Figure(size = (figure_span_x*72, figure_span_y*72) .* drawing_scale)
+    # figure = Figure(size = (options.drawing_size[1], options.drawing_size[2]), backgroundcolor=options.backgroundcolor)
+    # ax = Axis(figure[1, 1], aspect = figure_span_x/figure_span_y)
+
+    ax = Axis(figure[1, 1], backgroundcolor = options.backgroundcolor)
+
+
     hidedecorations!(ax)  # hides ticks, grid and lables
     hidespines!(ax)  # hide the frame
-    thickness_scale = maximum(t) * 72 * drawing_scale
-    linewidths = t ./ maximum(t) * thickness_scale
-    
-    attributes = (color=:grey, linestyle=:solid, linewidth=linewidths, marker=:circle, markersize=0)
-    [show_element_deformed_shape!(ax, cross_section_coords[i], Δ[i], deformation_scale, attributes, attributes.linewidth[i]) for i in eachindex(Δ)];
-   
+    # thickness_scale = maximum(t) * 72 * drawing_scale
+    # linewidths = t ./ maximum(t) * thickness_scale
 
-    # cross_section_mode_shape!(ax, model.elem, model.node, mode, n, deformation_scale, attributes);
+    # thickness_scale = maximum(t_segments) * 72 * drawing_scale
+    linewidths = t_segments .* options.thickness_scale
+
+    attributes = (color=options.linecolor, linestyle=options.linestyle, joinstyle=options.joinstyle)
+
+    # linesegment_ranges = LinesCurvesNodes.find_line_segments(linewidths)
+
+    # [show_element_deformed_shape!(ax, cross_section_coords[i], Δ[i], deformation_scale, attributes, attributes.linewidth[i]) for i in eachindex(Δ)];
+
+    [show_element_deformed_shape!(ax, coords_as_linesegments[i], Δ_as_linesegments[i], deformation_scale, attributes, linewidths[i]) for i in eachindex(Δ_as_linesegments)];
+
+    num_nodes = size(model.node)[1]
+    num_elem = size(model.elem)[1]
+
+    # if length(X_discretized) == length(t_discretized)  #closed section
+    if num_nodes == num_elem
+
+        coords_last_linesegment = Vector{Vector{Float64}}(undef, 3)
+        Δ_last_linesegment = Vector{Vector{Float64}}(undef, 3)
+
+        coords_last_linesegment[1] = coords_as_linesegments[end][end]
+        coords_last_linesegment[2] = coords_as_linesegments[1][1]
+        coords_last_linesegment[3] = coords_as_linesegments[1][2]
+
+        Δ_last_linesegment[1] = Δ_as_linesegments[end][end]
+        Δ_last_linesegment[2] = Δ_as_linesegments[1][1]
+        Δ_last_linesegment[3] = Δ_as_linesegments[1][2]
+
+        show_element_deformed_shape!(ax, coords_last_linesegment, Δ_last_linesegment, deformation_scale, attributes, linewidths[end])
+
+    end
     
     return ax, figure
 
 end
+
+
+
+
+# function minimum_mode_shape(model, eig, t, deformation_scale, drawing_scale, options, figure)
+    
+#     # x = model.node[:, 2]
+#     # y = model.node[:, 3]
+#     # Δx = abs(maximum(x) - minimum(x))
+#     # Δy = abs(maximum(y) - minimum(y))
+
+#     Pcr = Tools.get_load_factor(model, eig)
+#     mode_index = argmin(Pcr)
+#     mode = model.shapes[mode_index][:, eig]
+
+#     n = fill(5, length(t))
+
+#     cross_section_coords, Δ, figure_max_dims = cross_section_mode_shape_info(model.elem, model.node, mode, n, deformation_scale)
+   
+#     Δx = figure_max_dims[1]
+#     Δy = figure_max_dims[2]
+
+#     # figure = Figure(resolution = (Δx*72, Δy*72) .* drawing_scale)
+#     ax = Axis(figure[1, 1], aspect = Δx/Δy)
+#     hidedecorations!(ax)  # hides ticks, grid and lables
+#     hidespines!(ax)  # hide the frame
+#     thickness_scale = maximum(t) * 72 * drawing_scale
+#     linewidths = t ./ maximum(t) * thickness_scale
+    
+#     attributes = (color=:grey, linestyle=:solid, linewidth=linewidths, marker=:circle, markersize=0)
+#     [show_element_deformed_shape!(ax, cross_section_coords[i], Δ[i], deformation_scale, attributes, attributes.linewidth[i]) for i in eachindex(Δ)];
+   
+
+#     # cross_section_mode_shape!(ax, model.elem, model.node, mode, n, deformation_scale, attributes);
+    
+#     return ax, figure
+
+# end
 
 
 
@@ -336,6 +530,41 @@ function mode_shape(model, eig, mode_index, t, deformation_scale, drawing_scale)
     # cross_section_mode_shape!(ax, model.elem, model.node, mode, n, deformation_scale, attributes);
     
     return ax, figure
+
+end
+
+
+function get_deformed_shape_extents(model, eig, deformation_scale)
+
+        
+    Pcr = Tools.get_load_factor(model, eig)
+    mode_index = argmin(Pcr)
+    mode = model.shapes[mode_index][:, eig]
+    t = model.elem[:, 4]
+    n = fill(5, length(t))
+    cross_section_coords, Δ_nodes, figure_max_dim_range = cross_section_mode_shape_info(model.elem, model.node, mode, n, deformation_scale)
+
+    return figure_max_dim_range
+
+end
+
+function define_drawing_size(Δ, max_pixels)
+
+    max_index = argmax(Δ)
+    min_index = argmin(Δ)
+
+    if max_index == min_index
+        min_index = 2
+    end
+
+    drawing_size = Vector{Float64}(undef, 2)
+    
+    drawing_size[max_index] = max_pixels
+    drawing_size[min_index] = max_pixels * Δ[min_index] / Δ[max_index]
+
+    thickness_scale = (max_pixels / Δ[max_index])
+
+    return drawing_size, thickness_scale
 
 end
 
