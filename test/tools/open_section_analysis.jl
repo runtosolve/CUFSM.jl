5b4dbcd70fd083f74ac854cd8111176aaa1203e6
Cris Moen <cris.moen@runtosolve.com>
2025-01-23 18:11:56 -0500

    bump LinesCurvesNodes dependency

--- test/tools/open_section_analysis.jl
+++ test/tools/open_section_analysis.jl
@@ -1,4 +1,4 @@
-using CUFSM, CrossSectionGeometry, AISIS100
+using CUFSM, CrossSectionGeometry
 
 
 #AISI D100-17E Cold-Formed Steel Design Manual Volume 1
@@ -12,7 +12,7 @@ n = [3, 3, 3, 3, 3]
 n_r = [3, 3, 3, 3];
 θ = [π/2, π, -π/2, 0.0, π/2]
 
-section_geometry = CrossSection.Geometry.create_thin_walled_cross_section_geometry(section_dimensions, θ, n, r, n_r, t, centerline = "to left", offset = (section_dimensions[2], section_dimensions[3] - section_dimensions[1]))
+section_geometry = CrossSectionGeometry.create_thin_walled_cross_section_geometry(section_dimensions, θ, n, r, n_r, t, centerline = "to left", offset = (section_dimensions[2], section_dimensions[3] - section_dimensions[1]))
 
 E = 29500.0
 ν = 0.30
@@ -33,35 +33,35 @@ y_center = [section_geometry.center[i][2] for i in eachindex(section_geometry.ce
 
 model = CUFSM.Tools.open_section_analysis(x_center, y_center, t, lengths, E, ν, P, Mxx, Mzz, M11, M22, constraints, springs, neigs)
 
-scale = (1.0, 1.0)
-eig = 1
-ax, figure = CUFSM.Show.signature_curve(model, eig, scale)
-figure
+# scale = (1.0, 1.0)
+# eig = 1
+# ax, figure = CUFSM.Show.signature_curve(model, eig, scale)
+# figure
 
-drawing_scale = 1.0
-eig = 1
-deformation_scale = (1.0, 1.0)
-mode_index = 3
-ax, figure = CUFSM.Show.mode_shape(all_models[i], eig, mode_index, data.t[i]*ones(Float64, size(all_models[i].elem)[1]), deformation_scale, drawing_scale)
-figure
+# drawing_scale = 1.0
+# eig = 1
+# deformation_scale = (1.0, 1.0)
+# mode_index = 3
+# ax, figure = CUFSM.Show.mode_shape(all_models[i], eig, mode_index, data.t[i]*ones(Float64, size(all_models[i].elem)[1]), deformation_scale, drawing_scale)
+# figure
 
 
 
-CorZ = 0
-θ_top = 90.0
-#Calculate top flange + lip section properties.
-CorZ = 0
+# CorZ = 0
+# θ_top = 90.0
+# #Calculate top flange + lip section properties.
+# CorZ = 0
 
-b = 1.625 - t
-d = 0.500 - t/2
-θ = 90.0
-ho = 3.625
-μ = 0.30
-E = 29500.0
-G = E / (2 * (1 + μ))
-kϕ = 0.0
-Af,Jf,Ixf,Iyf,Ixyf,Cwf,xof,xhf,yhf,yof = AISIS100.v16S3.table_2_3_3__1(CorZ,t,b,d,θ)
+# b = 1.625 - t
+# d = 0.500 - t/2
+# θ = 90.0
+# ho = 3.625
+# μ = 0.30
+# E = 29500.0
+# G = E / (2 * (1 + μ))
+# kϕ = 0.0
+# Af,Jf,Ixf,Iyf,Ixyf,Cwf,xof,xhf,yhf,yof = AISIS100.v16S3.table_2_3_3__1(CorZ,t,b,d,θ)
 
-#Calculate the purlin distortional buckling half-wavelength.
-Lm = 999999999.0
-Lcrd = AISIS100.v16S3.appendix2_2_3_3_1__7(ho, μ, t, Ixf, xof, xhf, Cwf, Ixyf, Iyf)
+# #Calculate the purlin distortional buckling half-wavelength.
+# Lm = 999999999.0
+# Lcrd = AISIS100.v16S3.appendix2_2_3_3_1__7(ho, μ, t, Ixf, xof, xhf, Cwf, Ixyf, Iyf)
